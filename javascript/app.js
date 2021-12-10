64ebd6207a643a3e64b9cc7c9d596f3e4f447785
natherma <65506524+natherma@users.noreply.github.com>
2021-12-10 15:16:47 +0530

    little modification

--- javascript/app.js
+++ javascript/app.js
@@ -18,6 +18,11 @@ else
 }
 
 
+// pattern check
+function patternCheck(box1,box2,box3)
+{
+    return (allBoxs[box1].innerText!==""&&allBoxs[box2].innerText!==""&&allBoxs[box3].innerText!=="")&&(allBoxs[box1].innerText===allBoxs[box2].innerText&&allBoxs[box2].innerText===allBoxs[box3].innerText)
+}
 
 
 // check win function
@@ -25,41 +30,41 @@ let checkWin = () =>
 {
     setTimeout(()=>
     {
-        if((allBoxs[0].innerText!==""&&allBoxs[1].innerText!==""&&allBoxs[2].innerText!=="")&&(allBoxs[0].innerText===allBoxs[1].innerText&&allBoxs[1].innerText===allBoxs[2].innerText))
+        if(patternCheck(0,1,2))
         {        
             alert(`${allBoxs[0].innerText} is winner`);
             window.location.reload();   
         }
-        else if((allBoxs[3].innerText!==""&&allBoxs[4].innerText!==""&&allBoxs[5].innerText!=="")&&(allBoxs[3].innerText===allBoxs[4].innerText&&allBoxs[4].innerText===allBoxs[5].innerText))
+        else if(patternCheck(3,4,5))
         {        
             alert(`${allBoxs[3].innerText} is winner`);
             window.location.reload();   
         }
-        else if((allBoxs[6].innerText!==""&&allBoxs[7].innerText!==""&&allBoxs[8].innerText!=="")&&(allBoxs[6].innerText===allBoxs[7].innerText&&allBoxs[7].innerText===allBoxs[8].innerText))
+        else if(patternCheck(6,7,8))
         {        
             alert(`${allBoxs[6].innerText} is winner`);
             window.location.reload();   
         }
-        else if((allBoxs[0].innerText!==""&&allBoxs[3].innerText!==""&&allBoxs[6].innerText!=="")&&(allBoxs[0].innerText===allBoxs[3].innerText&&allBoxs[3].innerText===allBoxs[6].innerText))
+        else if(patternCheck(0,3,6))
         {        
             alert(`${allBoxs[0].innerText} is winner`);
             window.location.reload();   
         }
-        else if((allBoxs[1].innerText!==""&&allBoxs[4].innerText!==""&&allBoxs[7].innerText!=="")&&(allBoxs[1].innerText===allBoxs[4].innerText&&allBoxs[4].innerText===allBoxs[7].innerText))
+        else if(patternCheck(1,4,7))
         {        
             alert(`${allBoxs[1].innerText} is winner`);
             window.location.reload();   
         }
-        else if((allBoxs[2].innerText!==""&&allBoxs[5].innerText!==""&&allBoxs[8].innerText!=="")&&(allBoxs[2].innerText===allBoxs[5].innerText&&allBoxs[5].innerText===allBoxs[8].innerText))
+        else if(patternCheck(2,5,8))
         {        
             alert(`${allBoxs[2].innerText} is winner`);   
         }
-        else if((allBoxs[0].innerText!==""&&allBoxs[4].innerText!==""&&allBoxs[8].innerText!=="")&&(allBoxs[0].innerText===allBoxs[4].innerText&&allBoxs[4].innerText===allBoxs[8].innerText))
+        else if(patternCheck(0,4,8))
         {        
             alert(`${allBoxs[0].innerText} is winner`);
             window.location.reload();   
         }
-        else if((allBoxs[2].innerText!==""&&allBoxs[4].innerText!==""&&allBoxs[6].innerText!=="")&&(allBoxs[2].innerText===allBoxs[4].innerText&&allBoxs[4].innerText===allBoxs[6].innerText))
+        else if(patternCheck(2,4,6))
         {        
             alert(`${allBoxs[2].innerText} is winner`);
             window.location.reload();   
